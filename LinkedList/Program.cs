ee61e53eb1fac89a41de0c8da032198879922526
sayalipagar <pagarsayali565@gmail.com>
2022-09-17 10:38:07 +0530

    [sayali] Add:CreateLinkedList

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To LinkedList Problem Statement!!!!");
+            Linklist list = new Linklist();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nSelect Your Program: \n 1.CreateLinkedList");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+            }
         }
     }
-}
+    }
